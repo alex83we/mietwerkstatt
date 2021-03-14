e399a3b1eece13b88355088cb644b61acb8d39eb
aguth <aguthmann83@gmail.com>
2021-03-14 11:13:04 +0100

    Logo angepasst

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark ttf-border-bottom">
 {{--    <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name') }}</a>--}}
     <a class="navbar-brand" href="{{ url('/') }}">
-        <img src="/images/logo.svg" type="image/svg+xml" style="width:auto; height: 30px;">
+        <img src="/images/logo.svg" type="image/svg+xml" style="width:auto; height: 50px;">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
